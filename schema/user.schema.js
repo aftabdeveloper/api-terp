958f1f9c3810ba805fa46d15ac12626a1e00c48e
Aftab Alam <alam69745@gmail.com>
2023-12-18 19:14:30 +0530

    user schema and controller

---
+++ schema/user.schema.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+const {Schema} = mongoose
+const userSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+userSchema.index({ email: 1}, { unique: true })
+
+module.exports = mongoose.model("User",userSchema)
